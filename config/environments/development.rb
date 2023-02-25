4435c10157c9dc4ea19e3967caccc71c73d2a132
SudharshanaBabu <sudharshana.b@rorwebcoders.com>
2023-02-25 21:51:45 +0530

    added login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,27 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "rorwebcoderstechnology@gmail.com",
+    password: "ejuqmzohkpqjnfqe",
+  }
+
+
+ config.action_controller.asset_host = 'http://localhost:3000'
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ 
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
+ 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
   # Don't care if the mailer can't send.
   
   config.action_mailer.raise_delivery_errors = false
